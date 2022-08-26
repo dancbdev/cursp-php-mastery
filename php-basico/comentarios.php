e73f900678cbeb8c657c0e0bbc6d24aa7e5753e5
Daniel Cleber Batista <danielcb@ciandt.com>
2022-08-26 19:16:10 -0300

    Exemplos de comentários em php.

---
+++ php-basico/comentarios.php
@@ -0,0 +1,12 @@
+<?php
+
+# Este é um comentário de uma única linha...
+
+// Um outro comentário de uma única linha ...
+
+echo "Comentários!" . PHP_EOL;
+
+/*
+Este é um comentário que irá
+abramger mais de uma linha ...
+*/
