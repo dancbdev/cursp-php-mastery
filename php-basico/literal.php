3e092e76ed9017fdcd0c0281d9a442ec6b232808
Daniel Cleber Batista <danielcb@ciandt.com>
2022-08-26 19:25:38 -0300

    Exemplo do uso de valores literais.

---
+++ php-basico/literal.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+  Trabalhar com valores literais,
+  significa utilizar o valor bruto,
+  sem o uso de uma variável a que esse valor
+  seria atribuído.
+*/
+
+echo "Olá mundo!" . PHP_EOL;
+
+echo 10 . PHP_EOL;
+
+echo 10.99 . PHP_EOL;
