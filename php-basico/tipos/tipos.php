89d91a424cc68bdbc4d8aa74953781ce2bcd9fc7
Daniel Cleber Batista <danielcb@ciandt.com>
2022-08-26 19:43:08 -0300

    Exemplos dos tipos em PHP.

---
+++ php-basico/tipos/tipos.php
@@ -0,0 +1,17 @@
+<?php
+
+// Tipos no PHP
+
+// Inteiro
+echo 1000 . PHP_EOL;
+
+// String
+echo "Olá mundo!" . PHP_EOL;
+
+// Booleano
+echo true . PHP_EOL; // retorna 1
+echo false . PHP_EOL; // não tem nenhum retorno
+
+// Ponto Flutuante
+echo 19.99 . PHP_EOL;
+echo 1099.90 . PHP_EOL;
